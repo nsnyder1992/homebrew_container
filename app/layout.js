7bdccb252c771e04c44cc8a820227c0acc551d8d
Nick Snyder <nicholas.snyder@accruent.com>
2024-06-27 13:40:10 -0500

    updating layout

--- app/layout.js
+++ app/layout.js
@@ -18,8 +18,8 @@ export default function RootLayout({ children }) {
       <body>
         <Navbar />
         <section>
-          <div className="max-w-6xl mx-auto px-4 sm:px-6">
-            <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+          <div className="max-w-6xl mx-auto px-2 sm:px-4">
+            <div className="pt-15 pb-12 md:pt-20 md:pb-20">
               <div className="max-w-3xl mx-auto lg:max-w-none">
                 <div
                   className="lg:flex lg:justify-between"
--- app/page.js
+++ app/page.js
@@ -1,20 +1,17 @@
 import path from "path";
-// import { getSortedData } from "../lib/posts.mjs";
+import { getSortedData } from "../lib/posts.mjs";
 
 import Link from "next/link";
 
-export default function Home({ allRulesData, allCampaignsData }) {
+export default function Home() {
+  const allRulesData = getLinks();
+  console.log(allRulesData);
   return (
     <div>
       <section className="grid grid-cols-1 gap-4 m-10 justify-center">
         <p className="">
-          Homebrew Container is a container to post TTRPG rules using markdown
-          notes or MDX notes from a CMS, like obsidian
-        </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>
-          .)
+          Homebrew Container is a container to post MDX notes from a CMS, like
+          obsidian
         </p>
       </section>
       <section className="grid grid-cols-1 gap-4 m-10">
@@ -37,36 +34,11 @@ export default function Home({ allRulesData, allCampaignsData }) {
           )}
         </ul>
       </section>
-      <section className="grid grid-cols-1 gap-4 m-10">
-        <h2 className="text-3xl font-extrabold leading-tight tracking-tighter">
-          Campaigns
-        </h2>
-        <ul>
-          {allCampaignsData ? (
-            allCampaignsData.map(({ id, date, title }) => (
-              <li key={id} className="list-disc list-inside">
-                <Link href={`campaigns/${id}`}>{title}</Link>
-              </li>
-            ))
-          ) : (
-            <></>
-          )}
-        </ul>
-      </section>
     </div>
   );
 }
 
-// const campaignsDirectory = path.join(process.cwd(), "./pages/campaigns");
-// const rulesDirectory = path.join(process.cwd(), "./pages/rules");
-// Home.getInitialProps = async () => {
-//   const allRulesData = getSortedData(rulesDirectory);
-//   // const allCampaignsData = getSortedData(campaignsDirectory);
-
-//   return {
-//     props: {
-//       allRulesData,
-//       // allCampaignsData,
-//     },
-//   };
-// };
+const rulesDirectory = path.join(process.cwd(), "./app/rules");
+function getLinks() {
+  return getSortedData(rulesDirectory);
+}
