d09b0e9953e85064f295628284ef9a6ac6c99def
Nick Snyder <nicholas.snyder@accruent.com>
2024-06-28 09:08:37 -0500

    adding dice component

--- components/ttrpg/Dice.js
+++ components/ttrpg/Dice.js
@@ -1,3 +1,27 @@
-export default function Dice() {
-  return <p>dice!</p>;
+import { useState } from "react";
+
+export default function Dice({ numDice = 1, diceArray }) {
+  let [values, setValues] = useState([]);
+
+  let onRoll = () => {
+    let temp = [];
+    for (let i of numDice) {
+      let index = Math.floor(Math.random() * (diceArray - 1 - 0 + 1));
+      temp.push(diceArray[index]);
+    }
+    setValues(values);
+  };
+
+  return (
+    <div>
+      <button onClick={onRoll}>Roll!</button>
+      <p>
+        {values?.map((v) => {
+          return v + "   ";
+        })}
+        {" total:  "}
+        total: {values.reduce((partialSum, a) => partialSum + a, 0)}
+      </p>
+    </div>
+  );
 }
